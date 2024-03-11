7574ccfe3a948f109a451d0106660f637c67ec67
calvintseng <calvin.tseng46@gmail.com>
2024-03-11 01:24:58 -0500

    added q q

--- snake.c
+++ snake.c
@@ -1,13 +1,6 @@
 #include "snake.h"
 #include "apple.h"
-
-#define WINDOW_X 0
-#define WINDOW_Y 0
-#define WINDOW_WIDTH 1000
-#define WINDOW_HEIGHT 1000
-
-#define GRID_SIZE 20
-#define GRID_DIM 500
+#include "config.h"
 
 enum {
 	SNAKE_UP,
